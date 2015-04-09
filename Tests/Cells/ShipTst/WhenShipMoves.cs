2a9b35ebb56f3131371a1c033ac53f47816921af
VioletTape <my@violet-tape.net>
2015-04-09 17:45:10 +0300

    missed file

---
+++ Tests/Cells/ShipTst/WhenShipMoves.cs
@@ -0,0 +1,29 @@
+﻿using Core.BaseTypes;
+using Core.Cells;
+using Core.Enums;
+using Core.Extensions;
+using NUnit.Framework;
+
+namespace Tests.Cells.ShipTst {
+	[TestFixture]
+	public class WhenShipMoves {
+		[SetUp]
+		public void TestInit() {}
+
+		[Test]
+		public void OldCellShouldReleaseShip() {
+			// arrange
+			var originalCell = new WaterCell(1,1);
+			var ship = new Ship(PlayerType.Black, originalCell);
+
+			// Act
+			var newCell = new WaterCell(1,2);
+			ship.MoveTo(newCell);
+
+			// Assert
+			originalCell.Ship.ShouldBeNull();
+			newCell.Ship.ShouldBeNotNull();
+
+		}
+	}
+}
